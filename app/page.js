26a54bae0b225e822d50bf243b7345f364f8e09a
Khalid Bouychou <khalidbouychou22@gmail.com>
2024-10-11 11:42:38 +0100

    update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <Head>
-        <title>Khali - Portfolio</title>
+        <title>Khalid - Portfolio</title>
         <meta name="description" content="Portfolio of Your Name" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
